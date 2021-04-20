b893e4319fdd60eaccbd07c7030198ba2fd5ccdc
Miharu-380 <38t.ajlkk@gmail.com>
2021-04-20 06:14:51 +0000

    [Add]4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,21 @@
 class BooksController < ApplicationController
+  
+  def index
+  end
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def destroy
+  end
+  
 end
