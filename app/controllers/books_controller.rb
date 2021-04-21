b526aa96a0d66f9333a3424a8ebc6128ca919a1c
Miharu-380 <38t.ajlkk@gmail.com>
2021-04-21 04:50:03 +0000

    [Add2]4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,13 @@ class BooksController < ApplicationController
   
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to book_path(book.id)
+    else
+      flash.now[:alert] = "Error"
+      render :index
+    end
   end
   
   def show
@@ -21,8 +26,13 @@ class BooksController < ApplicationController
   
   def update
     book = Book.find(params[:id])
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to book_path(book.id)
+    else
+      flash.now[:alert] = "Error"
+      render :show
+    end
   end
   
   def destroy
